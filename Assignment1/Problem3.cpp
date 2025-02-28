fd921cc7d0fdc67269a505a06c6195a95d612a88
HeshamElsaman <hesham_ahmed_@outlook.com>
2025-02-28 12:58:15 -0500

    I hope I passed (part 2)

--- Assignment1/Problem3.cpp
+++ Assignment1/Problem3.cpp
@@ -15,23 +15,28 @@ template <typename T>
 void find_max(const std::string &type_name) {
     T n = 0, result = 0, prev = 0;
     
+    std::cout << "Sequence: ";
+    
     while (true) {
         result = series<T>(n);
         if (result > prev) {
             prev = result;
+            std::cout << prev << ", ";
             n++;
         } else {
             break;
         }
     }
+    
+    std::cout << std::endl;
 
     std::cout << "The maximum for " << type_name << " is: " << prev << std::endl;
     std::cout << "The maximum n for " << type_name << " is: " << n - 1 << std::endl << std::endl;
 }
 
 int main() {
-    find_max<int>("int");
-    find_max<long int>("long int");
+    find_max<signed int>("signed int");
+    find_max<signed long int>("signed long int");
     find_max<unsigned long int>("unsigned long int");
 
     return 0;
